06326832a4faf566b73832ae6af01fc33182bf64
Chris <chrisharwell72@gmail.com>
2020-05-08 01:19:04 -0400

    finished useDarkMode hook

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,6 +1,15 @@
-import React, {useEffect} from 'react';
-import {useLocalStorage} from './useLocalStorage';
+import React, { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
 
-export function useDarkMode() {
-    const [storedValue, setStoredValue] = useLocalStorage()
+export function useDarkMode(key, initialValue) {
+  const [storedValue, setStoredValue] = useLocalStorage(key, initialValue);
+
+  useEffect(() => {
+    if (initialValue) {
+      return <body className="dark-mode"></body>;
+    } else {
+        return <body className=""></body>;
+    }
+  }, [storedValue]);
+  return [setStoredValue, key, initialValue];
 }
